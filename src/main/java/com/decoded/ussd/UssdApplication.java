581779d3135bec052c405a9c60fb4c106d09422f
adams-okode <adamsokode@gmail.com>
2020-12-04 14:58:46 +0300

    first commit

---
+++ src/main/java/com/decoded/ussd/UssdApplication.java
@@ -0,0 +1,13 @@
+package com.decoded.ussd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UssdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UssdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decoded/ussd/UssdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decoded.ussd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UssdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
